e42da9dff711fe8605ae0bc998bcdfda3ff9f62a
Daniel M. Matongo <m_matongo@icloud.com>
2021-11-16 10:32:05 +0200

    update person logic

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 require_relative 'corrector'
 
 class Person
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :id
+  attr_reader :rentals, :parent_permission
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
